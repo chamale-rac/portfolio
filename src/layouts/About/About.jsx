252d70bba17208fa792aa17a654206386d42fc3e
chamale-rac <samuel.chamale23@gmail.com>
2023-05-19 00:13:10 -0600

    Me phone version

--- src/layouts/About/About.jsx
+++ src/layouts/About/About.jsx
@@ -9,13 +9,27 @@ const About = () => {
       <div className={styles.about}>
         <div className={styles.about_text}>
           <p className="font-body">
-            I'm a 19 years old student of Computer Science at Universidad del
-            Valle de Guatemala. I'm passionate about web development, UI/UX, and
-            nobby illustration. I'm also a pianist and 'retired' cyclist.
+            I'm a 19 years old student of Computer Science at{' '}
+            <a
+              href="https://www.uvg.edu.gt/"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: 'black' }}
+            >
+              Universidad del Valle de Guatemala.
+            </a>
+          </p>
+          <p className="font-body">
+            I'm passionate about web development, UI/UX, and nobby illustration.
+          </p>
+          <p className="font-body">
+            Thanks, thats all you need to know about me.
           </p>
         </div>
         <div className={styles.about_images}>
           <img src="/about/me.jpeg" alt="me" />
+          <img src="/public/about/char.jpeg" alt="char" />
+          <img src="/public/about/text.jpeg" alt="text" />
         </div>
       </div>
     </Collapse>
