da5ea98e8820a698606514ff8281ac42d7e1f699
chamale-rac <samuel.chamale23@gmail.com>
2023-05-28 00:54:57 -0600

    Fix: hook scroll to with reduced movement based on height

--- src/hooks/useScrollToElement.js
+++ src/hooks/useScrollToElement.js
@@ -4,18 +4,12 @@ const useScrollToElement = (scrollUpVh) => {
   const elementRef = useRef(null)
 
   const scrollToElement = (id) => {
-    console.log(scrollUpVh)
-    console.log('scrollToElement')
     const element = document.getElementById(id)
-    console.log(element)
     if (element) {
       const elementTop =
         element.getBoundingClientRect().top + window.pageYOffset
-      console.log(elementTop)
-      window.scrollTo({ top: elementTop, behavior: 'smooth' })
       const scrollUpPx = Math.round((scrollUpVh * window.innerHeight) / 100)
-      // TODO this line is not working
-      window.scrollBy({ top: -20, behavior: 'smooth' })
+      window.scrollTo({ top: elementTop - scrollUpPx, behavior: 'smooth' })
     }
   }
 
